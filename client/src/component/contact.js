1e65a5c4d2fa67818da2b0088963dfaf4319f138
Gaurav Nagar <gauravnagar192@gmail.com>
2019-05-18 19:36:46 +0530

    contact page done

--- client/src/component/contact.js
+++ client/src/component/contact.js
@@ -1,4 +1,6 @@
 import React , { Component } from 'react';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faMobileAlt , faEnvelope , faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 class Contact extends Component {
@@ -8,20 +10,24 @@ class Contact extends Component {
         <div id="contact">
          <div id="contact-us">
           <div id="ct-h">CONTACT US</div>
-          <div className="ct-info">856-956-8569</div>
-          <div className="ct-info">Netsol@gmail.com</div>
-          <div className="ct-info">0-13, Ashok Marg, Panch Batti,
-          C Scheme, Ashok Nagar,Jaipur, Rajasthan 302001</div>
+          <div className="ct-info"> Phone :
+          856-956-8569</div>
+          <div className="ct-info">  Email :
+          Netsol@gmail.com</div>
+          <div className="ct-info"> Address :
+          0-13, Ashok Marg, Panch Batti,</div>
+          <div className="ct-info">C Scheme, Ashok Nagar,Jaipur,</div>
+          <div className="ct-info">Rajasthan 302001</div>
           <div id="ct-sl">FEEL FREE TO CONTACT NETSOL</div>
          </div>
          <div id="fdbk">
          <div id="fdbk-h">FEEDBACK</div>
          <form action=" ">
-          <label htmlFor="" id="fn">Your Name</label>
+          <label htmlFor="" id="fn">Name</label>
           <input type="text" id="f-name"/>
-          <label htmlFor="" id="fe">Your Email</label>
+          <label htmlFor="" id="fe">Email</label>
           <input type="text" id="f-email"/>
-          <label htmlFor="" id="fm">Your Message</label>
+          <label htmlFor="" id="fm">Message</label>
           <textarea name="" id="f-msg"></textarea>
           <input type="submit" id="f-submit" value="SEND"/>
          </form>
