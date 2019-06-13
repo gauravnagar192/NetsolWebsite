7f0809ff67f80bee882ca62e76ac7cf9a6a46627
Gaurav Nagar <gauravnagar192@gmail.com>
2019-06-13 13:40:49 +0530

    make it better

--- route/router.js
+++ route/router.js
@@ -1,78 +1,11 @@
-require('./modal/issue');
-require('./modal/offer');
-require('./modal/order');
-require('./modal/feedback');
-require('./modal/candidate');
+const router = require('express').Router();
+const candidate = require('../modal/candidate');
+const feedback = require('../modal/feedback');
+const issue = require('../modal/issue');
+const offer = require('../modal/offer');
+const order = require('../modal/order');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const fs = require('fs');
-const multer = require('multer');
-const crypto = require('crypto');
-const { check, validationResult } = require('express-validator/check');
-const port = process.env.PORT || 5000;
-const issue = mongoose.model('issue');
-const offer = mongoose.model('offer');
-const order = mongoose.model('order');
-const feedback = mongoose.model('feedback');
-const candidate = mongoose.model('candidate');
-const route = require('./route/router');
-const app = express();
-
-//const morgan = require('morgan');
-//for console log current requests
-//app.use(morgan('dev'));
-
-const storage = multer.diskStorage({
-
-// Note: You are responsible for creating the directory when providing destination as a function.
-// When passing a string, multer will make sure that the directory is created for
-   destination: function (req, file, callback) {
-     //Specify already self created folder path to save image
-     callback(null, __dirname + '/image')
-   },
-   filename: function (req, file, callback) {
-     callback(null, file.fieldname + Date.now())
-   }
-});
-
-
-
-var upload = multer({ storage: storage })
-
-//Map global promises
-mongoose.Promise = global.Promise;
-
-//connecting to mongodb
-mongoose.connect('mongodb+srv://gaurav:mongodbgaurav@netsol-degpc.mongodb.net/wifi?retryWrites=true&w=majority', { useNewUrlParser: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err))
-
-app.use(express.static(path.join(__dirname,'/public')));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-app.use(express.json());
-
-app.use('/',route);
-
-// remove this in development
-//Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'));
-  });
-}
-
-app.post('/upload',upload.single('image'),[
+router.post('/upload',upload.single('image'),[
   check('name','Name is required').not().isEmpty().not().isIn(['undefined']),
   check('title','Title is required').not().isEmpty().not().isIn(['undefined']),
   check('experience','Experience is required').not().isEmpty().not().isIn(['undefined'])
@@ -116,7 +49,7 @@ app.post('/upload',upload.single('image'),[
   }
 });
 
-app.post('/',[
+router.post('/',[
   check('query','Query is required').not().isEmpty().not().isIn(['undefined'])
 ], (req, res) => {
   const errors = validationResult(req);
@@ -141,7 +74,7 @@ app.post('/',[
    })
 })
 
-app.post('/order',[
+router.post('/order',[
   check('price').not().isEmpty().not().isIn(['undefined']),
   check('name','Name is required').not().isEmpty().not().isIn(['undefined']),
   check('phone','Phone is required').not().isEmpty().isInt().not().isIn(['undefined']),
@@ -167,7 +100,7 @@ app.post('/order',[
    .then(() => res.json({'success': 'true'}))
 })
 
-app.post('/feedback',[
+router.post('/feedback',[
   check('name','Name is required').not().isEmpty().not().isIn(['undefined']),
   check('email','Email is required').not().isEmpty().not().isIn(['undefined']),
   check('message','Message is required').not().isEmpty().not().isIn(['undefined'])
@@ -187,7 +120,7 @@ app.post('/feedback',[
    .then(() => res.json({'success': 'true'}))
 })
 
-app.put('/answer', (req, res) => {
+router.put('/answer', (req, res) => {
   issue.findOne({ "_id" : req.body.id })
   .then(Issue => {
     var len = Issue.answers.length;
@@ -201,13 +134,13 @@ app.put('/answer', (req, res) => {
   })
 })
 
-app.get('/answers/:id', (req, res) => {
+router.get('/answers/:id', (req, res) => {
   issue.findOne({ "_id" : req.params.id })
   .then(Issue => res.json(Issue))
   .catch(err => console.log("ISSUE NOT FOUND"))
 })
 
-app.get('/review', (req, res) => {
+router.get('/review', (req, res) => {
   var first = [] ;
   var  second = [];
   feedback.find({})
@@ -227,14 +160,14 @@ app.get('/review', (req, res) => {
   })
 })
 
-app.get('/issue', (req, res) => {
+router.get('/issue', (req, res) => {
   issue.find({})
    .then(issue => {
      res.json(issue)
    })
 })
 
-app.get('/query/:issue', (req, res) => {
+router.get('/query/:issue', (req, res) => {
   issue.find({ _id : req.params.issue})
    .then(issue => {
      res.json(issue)
@@ -244,7 +177,7 @@ app.get('/query/:issue', (req, res) => {
    })
 })
 
-app.get('/booking/:rupee', (req, res) => {
+router.get('/booking/:rupee', (req, res) => {
   offer.find({ RS : req.params.rupee })
    .then(offer => {
      res.json(offer)
@@ -252,8 +185,4 @@ app.get('/booking/:rupee', (req, res) => {
    .catch(()=> {
      console.log("SERVER ERROR");
    })
-})
-
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
 })
