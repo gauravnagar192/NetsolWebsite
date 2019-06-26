1e94242c39f0b6f9f7be72b99cb63163eb02c937
Gaurav Nagar <gauravnagar192@gmail.com>
2019-06-26 20:36:23 +0530

    make it better

--- server.js
+++ server.js
@@ -37,8 +37,6 @@ const storage = multer.diskStorage({
    }
 });
 
-
-
 var upload = multer({ storage: storage })
 
 //Map global promises
@@ -241,6 +239,16 @@ app.get('/booking/:rupee', (req, res) => {
      console.log("SERVER ERROR");
    })
 })
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
