04f0dfde89df0d9b0ecc0cffcc2095e5ea5ebbd8
Gaurav Nagar <gauravnagar192@gmail.com>
2019-09-11 15:37:58 +0530

    make it better

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-	module.exports= require('./key_prod');
+	module.exports = require('./key_prod');
 } else {
-	module.exports= require('./key_dev');
+	module.exports = require('./key_dev');
 }
